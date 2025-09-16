4e8f92d1ee364f3c1fcc426cd94638bfbb0007c8
Vitalik Moroz <vitalik.moroz2@gmail.com>
2025-09-16 23:53:11 +0200

    consume file events

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,55 @@
-import fastify from 'fastify'
-import { Server } from 'socket.io'
+import fastify from "fastify";
+import { Server } from "socket.io";
 
-const app = fastify({ logger: true })
+const app = fastify({ logger: true });
 const io = new Server(app.server, { cors: { origin: "*" } });
 
-app.get('/', async (_req, reply) => {
-  reply.send("Hello world")
-})
+app.get("/", async (_req, reply) => {
+  reply.send("Hello world");
+});
 
 app.ready((err) => {
   if (err) throw err;
 
-  io.on('connection', (socket) => {
-    console.info('Socket connected!', socket.id);
-    socket.on("hello", () => {
-      console.log("Hello");
-    });
+  io.on("connection", (socket) => {
+    console.info("Socket connected!", socket.id);
+
+    socket.on(
+      "created-file",
+      (payload: Record<string, any>, callback: Function) => {
+        console.log("Created file", payload);
+        callback("Success");
+      }
+    );
+
+    socket.on(
+      "deleted-file",
+      (payload: Record<string, any>, callback: Function) => {
+        console.log("Deleted file", payload);
+        callback("Success");
+      }
+    );
+
+    socket.on(
+      "modified-file",
+      (payload: Record<string, any>, callback: Function) => {
+        console.log("Modified file", payload);
+        callback("Success");
+      }
+    );
+
+    socket.on(
+      "renamed-file",
+      (payload: Record<string, any>, callback: Function) => {
+        console.log("Renamed file", payload);
+        callback("Success");
+      }
+    );
 
     socket.on("disconnect", () => {
       console.log("Disconnected!", socket.id);
-    })
-  })
-})
+    });
+  });
+});
 
-export default app
+export default app;
